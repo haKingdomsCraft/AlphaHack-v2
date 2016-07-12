c1967f0338ef38f8636e794df71da714dfe863f8
ArceusMatt <arceusthegod303@yahoo.com>
2016-07-12 10:52:13 -0400

    Create AlphaHack v2.js

---
+++ AlphaHack v2.js
@@ -0,0 +1,32 @@
+var MainActivity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+//Get language of client side minecraft
+var getLanguage = ModPE.getLanguage();
+
+//Android import
+var Button = android.widget.Button;
+var LinearLayout = android.widget.LinearLayout;
+var RelativeLayout = android.widget.RelativeLayout;
+var PopupWindow = android.widget.PopupWindow;
+var ScrollView = android.widget.ScrollView;
+var TextView = android.widget.TextView;
+var CheckBox = android.widget.CheckBox;
+var Toast = android.widget.Toast;
+var Runnable = java.lang.Runnable;
+var View = android.view.View;
+var ColorDrawable = android.graphics.drawable.ColorDrawable;
+var Color = android.graphics.Color;
+var Gravity = android.view.Gravity;
+var Intent = android.content.Intent;
+var Uri = android.net.Uri;
+var Switch = android.widget.Switch;
+var EditText = android.widget.EditText;
+var Dialog = android.app.Dialog;
+var WebView = android.webkit.WebView;
+var Alert = android.app.AlertDialog.Builder
+
+//Defines
+var GUI;
+var menu;
+
+ModPE.langEdit("menu.copyright", "AlphaHack v2 @ArceusMatt");
